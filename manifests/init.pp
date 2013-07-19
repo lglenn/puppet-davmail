bb4e7e277cdda5817a1ca42f430b8b4f0f9efcaa
Larry Glenn <yrral.glenn@gmail.com>
2013-07-19 14:12:58 -0400

    Add documentation for davmail class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,8 @@
+# Public: Install DavMail.
+#
+# Examples
+#
+#   include davmail
 class davmail {
   package { 'davmail':
     source   => 'http://downloads.sourceforge.net/project/davmail/davmail/4.3.3/DavMail-MacOSX-4.3.3-2146.app.zip',
